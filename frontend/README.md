b018778a678fb92de9a9219aab67d9e136b23d3f
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-06-03 08:46:27 +0900

    Init: React 프로젝트 세팅

---
+++ frontend/README.md
@@ -0,0 +1,11 @@
+## 우아한 노션 프론트엔드
+
+
+
+#### 시작하기
+
+```sh
+$ cd frontend
+$ yarn
+$ yarn start
+```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="App">우아한 노션</div>
+  );
+}
+
+export default App;
